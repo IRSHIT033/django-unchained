88c9685d915185b919ce94c38f5f6578e8553bf9
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-09-24 00:19:40 +0530

    feat: readme updated

--- core/core/urls.py
+++ core/core/urls.py
@@ -25,7 +25,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('custom_auth.urls')),
 ]
